c60f08b5499b10da52848a78334557accc6403f8
Muhammad Hamza <hamza3256@live.co.uk>
2024-04-02 17:17:29 +0100

    feat: add product name, category, price to main page

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,18 +1,54 @@
 "use client";
 
 import { Product } from "@/payload-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Skeleton } from "./ui/skeleton";
+import Link from "next/link";
+import { cn, formatPrice } from "@/lib/utils";
+import { PRODUCT_CATEGORIES } from "@/config";
 
 interface ProductListingProps {
   product: Product | null;
   index: number;
 }
 
-const ProductListing = ({}: ProductListingProps) => {
+const ProductListing = ({ product, index }: ProductListingProps) => {
   const [isVisible, setIsVisible] = useState<boolean>(false);
 
-  if (true) return <ProductPlaceHolder />;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, index * 75);
+
+    return () => clearTimeout(timer);
+  }, [index]);
+
+  if (!product || !isVisible) return <ProductPlaceHolder />;
+
+  const label = PRODUCT_CATEGORIES.find(
+    ({ value }) => value === product.category
+  )?.label;
+
+  if (isVisible && product) {
+    return (
+      <Link
+        className={cn("invisible h-full w-full cursor-pointer group/main", {
+          "visible animate-in fade-in-5": isVisible,
+        })}
+        href={`/product/${product.id}`}
+      >
+        <div className="flex flex-col w-full">
+          <h3 className="mt-4 font-medium text-sm text-gray-700">
+            {product.name}
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">{label}</p>
+          <p className="mt-1 font-medium text-sm text-gray-900">
+            {formatPrice(product.price)}
+          </p>
+        </div>
+      </Link>
+    );
+  }
 };
 
 const ProductPlaceHolder = () => {
