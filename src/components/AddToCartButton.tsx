79b6761a303560050511c62b788416624215ffaf
Muhammad Hamza <hamza3256@live.co.uk>
2024-04-02 20:38:13 +0100

    feat: complete implementation of product page

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -2,9 +2,12 @@
 
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import { useCart } from "@/hooks/use-cart";
+import { Product } from "@/payload-types";
 
-const AddToCartButton = () => {
+const AddToCartButton = ({ product }: { product: Product }) => {
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const { addItem } = useCart();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -17,6 +20,7 @@ const AddToCartButton = () => {
   return (
     <Button
       onClick={() => {
+        addItem(product);
         setIsSuccess(true);
       }}
       size="lg"
