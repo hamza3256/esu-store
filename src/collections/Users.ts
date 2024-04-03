23631c8aa76aab387584c42eb5f49d5a93df7ac2
Muhammad Hamza <hamza3256@live.co.uk>
2024-04-03 21:36:51 +0100

    feat: implement sign up email

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,3 +1,4 @@
+import { PrimaryActionEmailHtml } from "@/components/emails/PrimaryActionEmail";
 import { Access, CollectionConfig } from "payload/types";
 
 const adminsAndUser: Access = ({ req: { user } }) => {
@@ -11,7 +12,11 @@ export const Users: CollectionConfig = {
   auth: {
     verify: {
       generateEmailHTML: ({ token }) => {
-        return `<a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}">Verify email</a>`;
+        return PrimaryActionEmailHtml({
+          actionLabel: "Verify your account",
+          buttonText: "Verify Account",
+          href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`,
+        });
       },
     },
   },
