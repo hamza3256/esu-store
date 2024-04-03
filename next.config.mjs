2a6b46bbf98fc1aa37c65d39656a79aa1aaecba8
Muhammad Hamza <muhammad.hamza@eigertrading.com>
2024-04-04 00:42:10 +0100

    chore(build): update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // remotePatterns: [
-    //   {
-    //     hostname: "localhost",
-    //     pathname: "**",
-    //     port: "3000",
-    //     protocol: "http",
-    //   },
-    // ],
-    domains: ["localhost", "esu-store.vercel.app", "esustore.com"],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "esu-store.vercel.app",
+			},
+		],
+	},
 };
 
-export default nextConfig;
+module.exports = nextConfig;
