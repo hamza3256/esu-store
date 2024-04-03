bd54d9a0241f65cb27964712deb56f27b176f09d
Muhammad Hamza <hamza3256@live.co.uk>
2024-04-03 21:18:44 +0100

    feat: improve admin access only

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,4 +1,10 @@
-import { CollectionConfig } from "payload/types";
+import { Access, CollectionConfig } from "payload/types";
+
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === "admin") return true;
+
+  return { id: { equals: user.id } };
+};
 
 export const Users: CollectionConfig = {
   slug: "users",
@@ -10,17 +16,43 @@ export const Users: CollectionConfig = {
     },
   },
   access: {
-    read: () => true,
+    read: adminsAndUser,
     create: () => true,
+    update: ({ req }) => req.user.role === "admin",
+    delete: ({ req }) => req.user.role === "admin",
+  },
+  admin: {
+    hidden: ({ user }) => user.role !== "admin",
+    defaultColumns: ["id"],
   },
   fields: [
+    {
+      name: "products",
+      label: "Products",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "products",
+      hasMany: true,
+    },
+    {
+      name: "product_files",
+      label: "Product files",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "product_files",
+      hasMany: true,
+    },
     {
       name: "role", // "Super Admin", "Admins", "Users"
       defaultValue: "user",
       required: true,
-      // admin: {
-      //   condition: ({ req }) => req.user.role === "admin",
-      // },
+      admin: {
+        condition: ({ req }) => req.user.role === "admin",
+      },
       type: "select",
       options: [
         {
