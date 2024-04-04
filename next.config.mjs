7b8ec5033005ad903ecc10ee0847cf4a1d269ad2
Muhammad Hamza <hamza3256@live.co.uk>
2024-04-04 05:15:03 +0100

    chore: update next config with withPayload module

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,43 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+// 	images: {
+// 		remotePatterns: [
+// 			{
+// 				protocol: "http",
+// 				hostname: "localhost",
+// 			},
+// 			{
+// 				protocol: "https",
+// 				hostname: "esu-store.vercel.app",
+// 			},
+// 		],
+// 	},
+// };
+
+import { withPayload } from "@payloadcms/next-payload";
+import { fileURLToPath } from "url";
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "esu-store.vercel.app",
-			},
-		],
-	},
-};
+const nextConfig = withPayload(
+  {
+    images: {
+      domains: [
+        "localhost",
+        "esu-store.vercel.app",
+        process.env.NEXT_PUBLIC_SERVER_URL
+          ? process.env.NEXT_PUBLIC_SERVER_URL
+          : "",
+      ],
+    },
+  },
+  {
+    // Convert the URL to a file path and then resolve the path
+    configPath: path.resolve(
+      fileURLToPath(new URL(".", import.meta.url)),
+      "src/payload.config.ts"
+    ),
+  }
+);
 
 export default nextConfig;
