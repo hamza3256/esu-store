c95912538ed2d28bc50d38fa31de4f6f077e5b0f
Muhammad Hamza <muhammad.hamza@eigertrading.com>
2024-10-05 12:12:34 +0100

    chore: update payment status on order-confirmation page

--- src/components/PaymentStatus.tsx
+++ src/components/PaymentStatus.tsx
@@ -22,20 +22,29 @@ const PaymentStatus = ({ orderEmail, orderId, isPaid }: PaymentStatusProps) => {
   const router = useRouter();
 
   useEffect(() => {
-    if (data?.isPaid) router.refresh();
+    if (data?.isPaid) {
+      router.refresh();
+    }
   }, [data?.isPaid, router]);
 
   return (
     <div className="mt-16 grid grid-cols-2 gap-x-4 text-gray-600">
       <div>
-        <p className="font-medium text-gray-900">Email has been sent to</p>
-        <p>{orderEmail}</p>
+        <p className="font-medium text-gray-900">Order Status:</p>
+        <p
+          className={`text-sm font-semibold ${
+            isPaid ? "text-green-600" : "text-yellow-500"
+          }`}
+        >
+          {isPaid ? "Payment successful" : "Pending payment"}
+        </p>
       </div>
-
-      <div>
-        <p className="font-medium text-gray-900">Order Status</p>
-        <p>{isPaid ? "Payment successful" : "Pending payment"}</p>
-      </div>
+      {isPaid && (
+        <div>
+          <p className="font-medium text-gray-900">Email has been sent to:</p>
+          <p className="text-sm text-gray-700">{orderEmail}</p>
+        </div>
+      )}
     </div>
   );
 };
