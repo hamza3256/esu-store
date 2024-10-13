c87883fa0d3e1e6fe78b74890c22140ae30381d3
Muhammad Hamza <muhammad.hamza@eigertrading.com>
2024-10-13 23:58:52 +0100

    chore: only triggle pollOrderStatus for online payments

--- src/components/PaymentStatus.tsx
+++ src/components/PaymentStatus.tsx
@@ -8,24 +8,25 @@ interface PaymentStatusProps {
   orderEmail: string;
   orderId: string;
   isPaid: boolean;
+  isCOD?: boolean;
 }
 
-const PaymentStatus = ({ orderEmail, orderId, isPaid }: PaymentStatusProps) => {
+const PaymentStatus = ({ orderEmail, orderId, isPaid, isCOD }: PaymentStatusProps) => {
   const { data } = trpc.payment.pollOrderStatus.useQuery(
     { orderId },
     {
-      enabled: isPaid === false,
-      refetchInterval: (data) => (data?.isPaid ? false : 1000),
+      enabled: !isCOD && isPaid === false, // Disable polling if it's COD
+      refetchInterval: (data) => (data?.isPaid ? false : 1000), // Poll every second until payment is confirmed
     }
   );
 
   const router = useRouter();
 
   useEffect(() => {
-    if (data?.isPaid) {
+    if (data?.isPaid && !isCOD) {
       router.refresh();
     }
-  }, [data?.isPaid, router]);
+  }, [data?.isPaid, router, isCOD]);
 
   return (
     <div className="mt-16 grid grid-cols-2 gap-x-4 text-gray-600">
@@ -33,13 +34,17 @@ const PaymentStatus = ({ orderEmail, orderId, isPaid }: PaymentStatusProps) => {
         <p className="font-medium text-gray-900">Order Status:</p>
         <p
           className={`text-sm font-semibold ${
-            isPaid ? "text-green-600" : "text-yellow-500"
+            isCOD
+              ? "text-blue-600" // Status for COD
+              : isPaid
+              ? "text-green-600"
+              : "text-yellow-500"
           }`}
         >
-          {isPaid ? "Payment successful" : "Pending payment"}
+          {isCOD ? "Cash On Delivery" : isPaid ? "Payment successful" : "Pending payment"}
         </p>
       </div>
-      {isPaid && (
+      {(isPaid || isCOD) && (
         <div>
           <p className="font-medium text-gray-900">Email has been sent to:</p>
           <p className="text-sm text-gray-700">{orderEmail}</p>
