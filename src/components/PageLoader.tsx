64ebe6e7904b481bc2ccb76395e75fee7ce447ea
Muhammad Hamza <muhammad.hamza@eigertrading.com>
2024-10-19 16:28:50 +0100

    chore: position the page loader slightly up

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,7 +1,7 @@
 const PageLoader = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+    <div className="flex items-center justify-center min-h-screen -mt-20">
+      <div className="animate-spin rounded-full h-24 w-24 md:h-32 md:w-32 border-t-2 border-b-2 border-gray-900"></div>
     </div>
   );
 };
