34bf681e8f4969776d50caaf676fae9e0c401441
Muhammad Hamza <hamza3256@live.co.uk>
2025-04-13 12:29:26 +0100

    fix: adapt logo to payload theme

--- src/components/AdminLogo.tsx
+++ src/components/AdminLogo.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
+import { useTheme } from 'payload/components/utilities';
 
 const AdminLogo = () => {
+  const { theme } = useTheme();
+
   return (
     <div className="flex items-center justify-center">
       <Image
-        src="/esu-transparent-white.png"
+        src={theme === 'dark' ? "/esu-transparent-white.png" : "/esu-transparent.png"}
         alt="ESU Store Logo"
         width={150}
         height={50}
